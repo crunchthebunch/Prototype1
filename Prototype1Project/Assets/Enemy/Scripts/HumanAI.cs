f7580ffd33701ec7932d58833227ce30ead29168
John <chocolatehotfries@gmail.com>
2019-07-18 17:19:14 +1200

    Made AI human shoot a bit better

--- Prototype1Project/Assets/Enemy/Scripts/HumanAI.cs
+++ Prototype1Project/Assets/Enemy/Scripts/HumanAI.cs
@@ -119,7 +119,6 @@ public class HumanAI : MonoBehaviour
 
                         if (isEndingTurn)
                         {
-                            Shoot();
                             EndTurn();
                         }
                     }
