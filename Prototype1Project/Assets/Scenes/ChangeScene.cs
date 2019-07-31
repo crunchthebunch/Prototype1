b6914f2f36090e09dec3ad4b4390efb2c2275df6
Varun Naval <varun.naval@gmail.com>
2019-07-31 18:24:40 +1200

    Added health pick up model, main menu, UI, etc.

---
+++ Prototype1Project/Assets/Scenes/ChangeScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("Callum level");
+    }
+
+    public void quitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
--- Prototype1Project/Assets/Scripts/Player.cs
+++ Prototype1Project/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -336,6 +337,8 @@ public class Player : MonoBehaviour
             HP = 0;
             isDead = true;
             animator.SetBool("isDead", true);
+            
+            Invoke("LoseCondition", 5.0f);
         }
         else
         {
@@ -386,5 +389,10 @@ public class Player : MonoBehaviour
     void ResetPickUp()
     {
         animator.SetBool("isPickingUp", false);
+    }
+
+    void LoseCondition()
+    {
+        SceneManager.LoadScene("Lose Screen");
     }
 }
